7145f87cf5c3a61b00ae6213ae875144166c2ef0
jodi / jodlodi <jodlodi090@gmail.com>
2021-10-18 10:23:00 +0200

    Port to 1.17.1

--- src/main/java/io/github/jodlodi/mixin/MixinBedBlock.java
+++ src/main/java/io/github/jodlodi/mixin/MixinBedBlock.java
@@ -1,14 +1,14 @@
 package io.github.jodlodi.mixin;
 
-import net.minecraft.block.BedBlock;
-import net.minecraft.block.BlockState;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraft.world.World;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BedBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,10 +16,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BedBlock.class)
 public class MixinBedBlock {
-    @Inject(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;removeBlock(Lnet/minecraft/util/math/BlockPos;Z)Z"), cancellable = true)
-    private void useMixin(BlockState p_225533_1_, World p_225533_2_, BlockPos p_225533_3_, PlayerEntity p_225533_4_, Hand p_225533_5_, BlockRayTraceResult p_225533_6_, CallbackInfoReturnable<ActionResultType> cir){
+    @Inject(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;removeBlock(Lnet/minecraft/core/BlockPos;Z)Z"), cancellable = true)
+    private void useMixin(BlockState p_49515_, Level p_49516_, BlockPos p_49517_, Player p_49518_, InteractionHand p_49519_, BlockHitResult p_49520_, CallbackInfoReturnable<InteractionResult> cir){
         cir.cancel();
-        p_225533_4_.displayClientMessage(new TranslationTextComponent("block.minecraft.bed.wrong"), true);
-        cir.setReturnValue(ActionResultType.SUCCESS);
+        p_49518_.displayClientMessage(new TranslatableComponent("block.minecraft.bed.wrong"), true);
+        cir.setReturnValue(InteractionResult.SUCCESS);
     }
 }
