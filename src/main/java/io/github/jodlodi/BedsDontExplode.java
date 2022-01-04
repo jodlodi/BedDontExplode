96df7c6210822caa06160b92ebf08d8770867464
jodi / jodlodi <jodlodi090@gmail.com>
2022-01-05 00:10:29 +0100

    Update to 1.18.1

--- src/main/java/io/github/jodlodi/BedsDontExplode.java
+++ src/main/java/io/github/jodlodi/BedsDontExplode.java
@@ -9,27 +9,13 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(BedsDontExplode.MOD_ID)
 public class BedsDontExplode
 {
     public static final String MOD_ID = "bedsdontexplode";
-    private static final Logger LOGGER = LogManager.getLogger();
 
     public BedsDontExplode() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         MinecraftForge.EVENT_BUS.register(this);
-
-        bus.addListener(this::setup);
-        bus.addListener(this::doClientStuff);
-    }
-
-    private void setup(final FMLCommonSetupEvent event)
-    {
-
-    }
-
-    private void doClientStuff(final FMLClientSetupEvent event) {
-
     }
 }
